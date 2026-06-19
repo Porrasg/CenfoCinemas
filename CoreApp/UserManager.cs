0cb038f2b7efdbe1848009592f86f01781e15461
kianny_porras <kianny.valeriapg@gmail.com>
2026-06-18 19:13:27 -0600

    Api clase sem 6

---
+++ CoreApp/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.CRUD;
+using Entities_DTOs;
+
+namespace CoreApp
+{
+    // Logica de Negocio
+    public class UserManager
+    {
+        public List<User> RetrieveAllUsers()
+        {
+            var uCrud = new UserCrudFactory();
+            return uCrud.RetrieveAll<User>();
+        }
+
+    }
+}
---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoreApp;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("RetrieveAll")]
+        public ActionResult RetrieveAll()
+        {
+            try
+            {
+                var um = new UserManager();
+                var lstResults = um.RetrieveAllUsers();
+                return Ok(lstResults);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+    }
+}
+}
